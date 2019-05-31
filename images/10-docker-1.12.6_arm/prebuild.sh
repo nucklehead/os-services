5b356e6f0eae5f0bc7d3322e581a3d0447b3c2f8
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2019-05-31 11:14:53 -0300

    Update prebuild.sh

--- images/10-docker-1.12.6_arm/prebuild.sh
+++ images/10-docker-1.12.6_arm/prebuild.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-set -ex
+# patch version 1, CVE-2019-5736
+set -e
 
 #RUN curl -sfL https://get.docker.com/builds/Linux/x86_64/docker-1.11.2.tgz | tar xzf - -C /assets && \
 #    mv /assets/docker /assets/docker-1.11.2
 
 VERSION=$1
 ARCH=$2
-if [ "$ARCH" == "amd64" ]; then
-  ARCH="x86_64"
-  URL="https://get.docker.com/builds/Linux/${ARCH}/docker-${VERSION}.tgz"
-else
-  URL="https://github.com/rancher/docker-package/releases/download/docker-v${VERSION}/docker-v${VERSION}_${ARCH}.tgz"
-  SUFFIX="_${ARCH}"
+if [ "$ARCH" == "arm64" ]; then
+    URL="https://github.com/rancher/docker-package/releases/download/docker-v${VERSION}/docker-v${VERSION}_${ARCH}.tgz"
+    SUFFIX="_${ARCH}"
 fi
 
 DEST="./images/10-docker-${VERSION}${SUFFIX}"
@@ -19,4 +17,6 @@ DEST="./images/10-docker-${VERSION}${SUFFIX}"
 mkdir -p $DEST
 curl -sL ${URL} | tar xzf - -C $DEST
 mv $DEST/docker $DEST/engine
-#mv $DEST/completion $DEST
+rm -rf $DEST/completion
+
+curl -sL -o $DEST/engine/docker-runc https://github.com/rancher/runc-cve/releases/download/CVE-2019-5736-build2/runc-v${VERSION}-${ARCH}
